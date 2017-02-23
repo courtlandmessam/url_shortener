4d8b48886a454ff8553cffe44407d084ea2e611a
Courtland Messam <messamcourtland@gmail.com>
2017-02-23 11:39:06 -0500

    fixed redirect bug updated root file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :links
   get ':in_url' => 'links#go'
+  root 'links#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
